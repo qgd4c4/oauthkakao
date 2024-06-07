26624fdc3b0d35c96cd69286849a6ae721fe6c54
jhsong <jh@song>
2024-06-07 16:12:13 +0900

    SecurityConfig successHandler 추가

--- src/main/java/com/example/oauthkakao/config/SecurityConfig.java
+++ src/main/java/com/example/oauthkakao/config/SecurityConfig.java
@@ -2,23 +2,56 @@ package com.example.oauthkakao.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import com.example.oauthkakao.service.OAuth2UserService;
+
+import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
+ 
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
-
-@Bean
-public SecurityFilterChain SecurityFilterChain(HttpSecurity http) throws Exception {
-
-    http.oauth2Login(login -> login
-                                .loginPage("/login")
-                                
-                );
-
-    return http.build();
-}
-
+    private final OAuth2UserService oAuth2UserService;
+ 
+    public SecurityConfig(OAuth2UserService oAuth2UserService) {
+        this.oAuth2UserService = oAuth2UserService;
+    }
+ 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeHttpRequests(config -> config.anyRequest().permitAll());
+        http.oauth2Login(oauth2Configurer -> oauth2Configurer
+                        .loginPage("/login")
+                        .successHandler(successHandler())
+                        .userInfoEndpoint()
+                        .userService(oAuth2UserService));
+ 
+        return http.build();
+    }
+ 
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        return ((request, response, authentication) -> {
+            DefaultOAuth2User defaultOAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
+ 
+            String id = defaultOAuth2User.getAttributes().get("id").toString();
+            String body = """
+                    {"id":"%s"}
+                    """.formatted(id);
+ 
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+ 
+            PrintWriter writer = response.getWriter();
+            writer.println(body);
+            writer.flush();
+        });
+    }
 }
