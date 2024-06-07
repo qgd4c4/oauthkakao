7e1f615ca0b4974296d484008111ef9e256180f5
jhsong <jh@song>
2024-06-07 15:08:46 +0900

    first commit

---
+++ src/main/java/com/example/oauthkakao/OauthkakaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthkakao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthkakaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthkakaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oauthkakao/OauthkakaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthkakao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthkakaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
